e38384677f95d1145a357d786964fec76c790a12
quarkusio <no-reply@quarkus.io>
2024-08-31 13:17:14 +0000

    Initial commit

---
+++ src/test/java/org/acme/vt/GreetingResourceIT.java
@@ -0,0 +1,8 @@
+package org.acme.vt;
+
+import io.quarkus.test.junit.QuarkusIntegrationTest;
+
+@QuarkusIntegrationTest
+class GreetingResourceIT extends GreetingResourceTest {
+    // Execute the same tests but in packaged mode.
+}
